d313d29e560feef4649fe11a2b7e1195a4a35cae
FixedAmateur <12a1tranphamtuan@gmail.com>
2024-12-03 01:09:17 +0700

    5

--- main.py
+++ main.py
@@ -22,8 +22,11 @@ def save_df(df, file_name, format):
         except Exception as e:
             print(f"An error occurred while replacing the file: {e}")
     else:
-        df.to_csv(file)
-        print(f"File '{file}' created successfully.")
+        try:
+            df.to_csv(file)
+            print(f"File '{file}' created successfully.")
+        except Exception as e:
+            print(f"An error occurred while creating the file: {e}")
 
 # 1.2. Reading csv file
 def read_csv_file(file_name):
@@ -49,17 +52,17 @@ print('\n' + file_name + ":\n", df, '\n')
 # 3. DATA PREPROCESSING
 print("PREPROCESS TRAINING DATA")
 # 3.1. Remove missing values and encode categorical variables
-def data_clean(df, file_name):
-    df = df.dropna()
+def data_clean(df, df_name):
 
+    df = df.dropna()
     df_category = df.select_dtypes(include=['object', 'string'])
-    print(f"Categorical variables of {file_name}:", list(df_category.columns), '\n')
+    print(f"Categorical variables of {df_name}:", list(df_category.columns), '\n')
     if list(df_category.columns):
         for column in df_category.columns:
             df[column] = LabelEncoder().fit_transform(df[column])
-
-        print("\nCategorical variables encoded " + file_name + ":\n", df, '\n')
-
+        print(f"\nCategorical variables encoded {df_name}:\n", df, '\n')
+    else:
+        print(f"\n{df_name} has no categorical variables")
     return df
 
 df = data_clean(df, file_name)
@@ -74,36 +77,36 @@ save_df(df_descriptive, f"{descriptive_dir}/Descriptive_{file_name}", "csv")
 
 # 3.3. Histogram chart
 def histogram(df, file_name):
-    hists = {}
+    hists = []
     for column in df:
         fig = plt.figure(figsize=(10,6)) #create frame of chart
         sns.histplot(df[column], kde=True) # insert data in frame
         plt.title(f"Distribution of {column} in {file_name}")
         plt.xlabel(column)
         plt.ylabel("Frequency")
-        hists[column] = fig
+        hists.append(fig)
     return hists
 
 histograms = histogram(df, file_name)
 os.makedirs("Histogram",exist_ok=True)
-for column, fig in histograms.items():
-    fig.savefig(f"Histogram/{column}.png")
+for fig in histograms:
+    fig.savefig(f"Histogram/{fig.axes[0].get_title()}.png")
 
 # 3.4. Boxplot chart
 def boxplot(df, file_name):
-    boxs = {}
+    boxs = []
     for column in df:
         fig = plt.figure(figsize=(10, 6))
         sns.boxplot(df[column])
         plt.title(f"Boxplot of {column} in {file_name}")
         plt.ylabel(column)
-        boxs[column] = fig
+        boxs.append(fig)
     return boxs
 
 os.makedirs("Boxplot",exist_ok=True)
 boxplots = boxplot(df, file_name)
-for column, fig in boxplots.items():
-    fig.savefig(f"Boxplot/{column}.png")
+for fig in boxplots:
+    fig.savefig(f"Boxplot/{fig.axes[0].get_title()}.png")
 
 # 3.4. Save preprocessed file
 save_df(df, f"Preprocessed_{file_name}", "csv")
@@ -116,26 +119,29 @@ features = [x for x in list(df.columns) if x != target_var]
 df_y = df[target_var]
 df_x = df[features]
 
-print(f"\n{file_name}'s feature columns:\n", list(df_x.columns), '\n')
+print(f"\n{file_name}'s feature columns:\n", features, '\n')
 print(f"\n{file_name}'s target variable:\n", [target_var], '\n')
 
 # 4.2. Split train and test dataframe
+evaluation_dir = "Model evaluation"
+split_dir = "Split file"
 df_x_train, df_x_test, df_y_train, df_y_test = train_test_split(df_x, df_y, test_size=0.2, random_state=42)
-os.makedirs("Splitted training file", exist_ok=True)
-save_df(df_x_train, f"Splitted training file/Splitted_features_of_{file_name}_for_training", "csv")
-save_df(df_x_test, f"Splitted training file/Splitted_features_of_{file_name}_for_testing","csv")
-save_df(df_y_train, f"Splitted training file/Splitted_target_of_{file_name}_for_training","csv")
-save_df(df_y_test, f"Splitted training file/Splitted_target_of_{file_name}_for_testing", "csv")
+os.makedirs(f"{evaluation_dir}/{split_dir}", exist_ok=True)
+save_df(df_x_train, f"{evaluation_dir}/{split_dir}/Split_features_of_{file_name}_for_training", "csv")
+save_df(df_x_test, f"{evaluation_dir}/{split_dir}/Split_features_of_{file_name}_for_testing","csv")
+save_df(df_y_train, f"{evaluation_dir}/{split_dir}/Split_target_of_{file_name}_for_training","csv")
+save_df(df_y_test, f"{evaluation_dir}/{split_dir}/Split_target_of_{file_name}_for_testing", "csv")
 
 # 4.3. Create model with splitted dataframes for training
 lr_model = LinearRegression().fit(df_x_train, df_y_train)
 print("Model created and trained!")
 
 # 4.4. Make prediction on splitted test dataframe
-df_y_predicted = lr_model.predict(df_x_test)
+df_y_predicted = pd.DataFrame(lr_model.predict(df_x_test), columns=["Predicted Performance Index"])
+save_df(df_y_predicted,f"{evaluation_dir}/{split_dir}/Prediction_of_{file_name}_split_features","csv" )
 
 # 4.4. Evaluate model
-evaluation_dir = "Model evaluation"
+
 os.makedirs(evaluation_dir, exist_ok=True)
 def evaluate_lr_model(lr_model, evaluation_dir, x_test, y_test, y_pred):
     evaluation = {
@@ -151,23 +157,23 @@ print("Model evaluation:\n",df_evaluation)
 
 # 4.5. Draw scatterplot
 def scatter(x_test, y_test, y_prediction):
-    scatters = {}
+    scatters = []
     target = y_test.name
     for column in x_test:
         fig = plt.figure(figsize=(10,8))
-        plt.scatter(x_test[column], y_test, color="blue", label=column)
-        #plt.plot(x_test[column], y_prediction, color="red", label=target)
+        plt.scatter(x_test[column], y_test, color="blue", label="Actual")
+        #plt.plot(x_test[column], y_prediction, color="red", label="Predicted")
         plt.title(f"Collaration between {column} and {target}")
         plt.xlabel(column)
         plt.ylabel(target)
         plt.legend()
-        scatters[column] = fig
+        scatters.append(fig)
 
     return scatters
 
 os.makedirs(f"{evaluation_dir}/Scatter", exist_ok=True)
 scatters = scatter(df_x_test, df_y_test, df_y_predicted)
-for column, fig in scatters.items():
+for fig in scatters:
     fig.savefig(f"{evaluation_dir}/Scatter/{fig.axes[0].get_title()}.png")
 
 
@@ -176,8 +182,12 @@ print("MAKE PREDICTION WITH INPUT CSV FILES")
 # 5.1. Create prediction dataframe from input csv file
 def create_prediction_df(test_name, lr_model, features, target_name):
     df_prediction = read_csv_file(test_name)
-    data_clean(df_prediction, test_name)
+    df_prediction = data_clean(df_prediction, test_name)
     x = df_prediction[features]
+    print(x)
+    if x.isnull().sum().sum() > 0:
+        print("Warning: The input contains NaN values after cleaning.")
+        return None
     y = np.around(lr_model.predict(x), decimals=2)
     df_prediction[target_name] = y
     return df_prediction
